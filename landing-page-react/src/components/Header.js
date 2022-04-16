886c9f7691e06682c15dfe8e1c11f048ef21140a
Brian Garton <gartonb@gmail.com>
2022-04-16 18:43:01 +0700

    header.css

--- landing-page-react/src/components/Header.js
+++ landing-page-react/src/components/Header.js
@@ -7,23 +7,26 @@ const Header = () => {
   return (
     <div className="header">
         <div className="container">
-            <div className="logo-container">
-                <a href="#">.my-world <FiDribbble /></a>
+            <div className="header-container">
+                <div className="logo-container">
+                        <a href="#">.my-world <FiDribbble /></a>
+                </div>
+                <ul className="menu">
+                    <li className="menu-link">
+                        <a href="#">PORTFOLIO</a>
+                    </li>
+                    <li className="menu-link">
+                        <a href="#">ABOUT</a>
+                    </li>
+                    <li className="menu-link">
+                        <a href="#">CONTACT</a>
+                    </li>
+                </ul>
+                <div className="mobile-menu">
+                    <a href="#">Mobile Menu</a>
+                </div>
             </div>
-            <ul className="menu">
-                <li className="menu-link">
-                    <a href="#">PORTFOLIO</a>
-                </li>
-                <li className="menu-link">
-                    <a href="#">ABOUT</a>
-                </li>
-                <li className="menu-link">
-                    <a href="#">CONTACT</a>
-                </li>
-            </ul>
-            <div className="mobile-menu">
-                <a href="#">Mobile Menu</a>
-            </div>
+            
         </div>
     </div>
   )
