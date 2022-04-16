32276ee11fa44bc54b8d66a9cefaaca0ac3e5f4d
Brian Garton <gartonb@gmail.com>
2022-04-16 19:09:48 +0700

    Header Done

--- landing-page-react/src/components/Header.js
+++ landing-page-react/src/components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { FiX, FiDribbble, FiMenu } from "react-icons/fi";
 
 
 const Header = () => {
+
+    const [click, setClick] = useState(false)
+   
+    // Handling click and changes its status
+    const handleClick = () =>{
+        // if click = false, click => true; 
+        // if click = true, click => false;
+        setClick(!click);   
+    }
+
+    // Closing Mobile menu
+    const closeMobileMenu = () =>{
+        setClick(false);
+    }
+
   return (
     <div className="header">
         <div className="container">
@@ -11,7 +26,7 @@ const Header = () => {
                 <div className="logo-container">
                         <a href="#">.my-world <FiDribbble /></a>
                 </div>
-                <ul className="menu">
+                <ul className="menu active">
                     <li className="menu-link">
                         <a href="#">PORTFOLIO</a>
                     </li>
@@ -22,8 +37,8 @@ const Header = () => {
                         <a href="#">CONTACT</a>
                     </li>
                 </ul>
-                <div className="mobile-menu">
-                    <a href="#">Mobile Menu</a>
+                <div className="mobile-menu" onClick = {handleClick}>
+                    {click ? (<FiX />) : (<FiMenu />)}
                 </div>
             </div>
             
