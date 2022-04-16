a113c4b745095d4ca88e8b1130a6457e8adef59d
Brian Garton <gartonb@gmail.com>
2022-04-16 18:21:47 +0700

    header-50% styled

--- landing-page-react/src/components/Header.js
+++ landing-page-react/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Header.css"
-import { IconName } from "react-icons/fi";
+import { FiX, FiDribbble, FiMenu } from "react-icons/fi";
 
 
 const Header = () => {
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="header">
         <div className="container">
             <div className="logo-container">
-                <a href="#">.my-world </a>
+                <a href="#">.my-world <FiDribbble /></a>
             </div>
             <ul className="menu">
                 <li className="menu-link">
@@ -21,6 +21,9 @@ const Header = () => {
                     <a href="#">CONTACT</a>
                 </li>
             </ul>
+            <div className="mobile-menu">
+                <a href="#">Mobile Menu</a>
+            </div>
         </div>
     </div>
   )
