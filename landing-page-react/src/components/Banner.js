1876632c40a8de1fc2d7ca4162b0b022bc3fbc5f
Brian Garton <gartonb@gmail.com>
2022-04-16 19:48:51 +0700

    banner

---
+++ landing-page-react/src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <div className="banner-des">
+                <h1>Welcome to the World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, aliquid consectetur et labore adipisci eveniet quae possimus pariatur id Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolores exercitationem nobis, incidunt commodi fuga corporis aliquam ipsam aperiam? Exercitationem totam perspiciatis vitae nulla? Aperiam ipsa ratione ullam animi quibusdam odio sed cum debitis sint? Aperiam expedita nulla placeat neque iure eveniet, accusantium modi reprehenderit quas doloremque exercitationem sequi iste.</p>
+                <button>Load more</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
