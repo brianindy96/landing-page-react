f8c30ac5f98dc0729734b9578ecad590764a7e66
Brian Garton <gartonb@gmail.com>
2022-04-16 18:11:45 +0700

    installed icons

--- landing-page-react/src/components/Header.js
+++ landing-page-react/src/components/Header.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import "./Header.css"
+import { IconName } from "react-icons/fi";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="container">
+            <div className="logo-container">
+                <a href="#">.my-world </a>
+            </div>
+            <ul className="menu">
+                <li className="menu-link">
+                    <a href="#">PORTFOLIO</a>
+                </li>
+                <li className="menu-link">
+                    <a href="#">ABOUT</a>
+                </li>
+                <li className="menu-link">
+                    <a href="#">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
