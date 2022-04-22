0e215a61d954f18089c8a5c0101d59bc9478c3d4
Brian Garton <gartonb@gmail.com>
2022-04-22 11:25:19 +0700

    Header Done

---
+++ shopping-template/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <header className="header-container">
+        <div className="logo-cont">
+            <h1>E-Commerce</h1>
+        </div>
+        <div className="links">
+            <ul>
+                <li><a href="#">Sign In</a></li>
+                <li><a href="#">Cart</a></li>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
